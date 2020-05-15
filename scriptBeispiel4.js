84148239e282f451813c92674da3db67a5efbd76
000jemand000 <000jemand000@gmail.com>
2020-05-15 11:59:37 +0200

    .

---
+++ scriptBeispiel4.js
@@ -0,0 +1,29 @@
+
+let cnt = 0;
+let username;
+
+function login(){
+    username = document.getElementById("txbx_usrname").value + ":";
+    //txfield.removeChild(document.getElementById("p1"));
+    document.getElementById("loginSection").style.display = "none";
+    document.getElementById("messageSection").style.display = "block";
+}
+
+function sendMsg(){
+    let msgList = document.getElementById("messages");
+    let msgText = document.getElementById("txbx_eingabe").value;
+    cnt++;
+    let mkbox = document.createElement("ueberbox");
+        mkbox.id = "box_" + cnt;
+        msgList.appendChild(mkbox);
+    let box = document.getElementById("box_" + cnt);
+    let userLabel = document.createElement("user-label");
+        userLabel.id="user_" + cnt;
+        box.appendChild(userLabel);
+    let message = document.createElement("message-box");
+        message.id="message_" + cnt;
+        box.appendChild(message);
+        document.getElementById("user_" + cnt).innerHTML = username;
+        document.getElementById("message_" + cnt).innerHTML = msgText;
+        document.getElementById("txbx_eingabe").value = "";
+}
